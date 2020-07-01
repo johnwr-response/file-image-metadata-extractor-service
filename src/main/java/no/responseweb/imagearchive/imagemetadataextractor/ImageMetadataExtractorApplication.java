20fa92f100e3f4d558a1ec7f7c6c34eaf99efbe8
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-07-01 18:05:19 +0200

    Init

---
+++ src/main/java/no/responseweb/imagearchive/imagemetadataextractor/ImageMetadataExtractorApplication.java
@@ -0,0 +1,17 @@
+package no.responseweb.imagearchive.imagemetadataextractor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ImageMetadataExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageMetadataExtractorApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/no/responseweb/imagearchive/imagemetadataextractor/ImageMetadataExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package no.responseweb.imagearchive.imagemetadataextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageMetadataExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
